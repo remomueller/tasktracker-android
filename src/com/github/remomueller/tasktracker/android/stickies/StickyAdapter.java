3d9c32b6fb638d7e58c9075d394c12375136283f
Remo Mueller <remosm@gmail.com>
2012-08-02 10:59:25 -0400

    Fixed stickies displayed on index from being overwritten by other stickies on scroll

--- src/com/github/remomueller/tasktracker/android/stickies/StickyAdapter.java
+++ src/com/github/remomueller/tasktracker/android/stickies/StickyAdapter.java
@@ -35,7 +35,7 @@ public class StickyAdapter extends BaseAdapter {
     }
 
     public Object getItem(int position) {
-        return position;
+        return data.get(position);
     }
 
     public long getItemId(int position) {
@@ -44,8 +44,9 @@ public class StickyAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         View vi=convertView;
-        if(convertView==null)
+        if(convertView==null){
             vi = inflater.inflate(R.layout.stickies_index_item, null);
+        }
 
         TextView sticky_id = (TextView) vi.findViewById(R.id.sticky_id);
         TextView description = (TextView) vi.findViewById(R.id.description);
@@ -62,19 +63,22 @@ public class StickyAdapter extends BaseAdapter {
         sticky_id.setText(Integer.toString(sticky.id));
         if(sticky.completed == true){
             sticky_id.setPaintFlags(sticky_id.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        }else{
+            sticky_id.setPaintFlags(sticky_id.getPaintFlags() & (~ Paint.STRIKE_THRU_TEXT_FLAG));
         }
+
         description.setText(sticky.full_description());
         due_date.setText(sticky.short_due_date());
 
         // final TextView tagtext = new TextView(vi);
-        int i;
 
-        if(sticky.tags.length != 0){ // Not sure why this line is necessary, but it keeps stickies with no tags from showing tags from other stickies...
-            for(i = 0; i < sticky.tags.length; i++){
+        if(sticky.tags.length != 0){
+            for(int i = 0; i < sticky.tags.length; i++){
                 // final TextView tagtext = new TextView(vi);
                 if(i == 0){
                     single_tag.setText(sticky.tags[i].name);
                     single_tag.setBackgroundColor(Color.parseColor(sticky.tags[i].color));
+                    single_tag.setVisibility(View.VISIBLE);
                 }
                 // tagtext.setText("This is: " + sticky.tags[i].name);
                 // tags_container.addView(tagtext);
